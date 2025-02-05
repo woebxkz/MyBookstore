74dceee83997b07599e9fe9ae750dee76487ef4d
weron <weronika.jozwiak19@gmail.com>
2025-02-05 22:17:49 +0100

    user service and controller

--- src/main/java/shop/mybookstore/controller/UserController.java
+++ src/main/java/shop/mybookstore/controller/UserController.java
@@ -1,12 +1,28 @@
 package shop.mybookstore.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import shop.mybookstore.entity.User;
+import shop.mybookstore.response.ApiResponse;
+import shop.mybookstore.service.UserService;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
+
+    UserService userService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(new ApiResponse("List of all users: ", users));
+    }
+
 
 }
--- src/main/java/shop/mybookstore/service/UserService.java
+++ src/main/java/shop/mybookstore/service/UserService.java
@@ -2,14 +2,14 @@ package shop.mybookstore.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import shop.mybookstore.entity.Cart;
-import shop.mybookstore.entity.Order;
 import shop.mybookstore.entity.RoleEnum;
 import shop.mybookstore.entity.User;
-import shop.mybookstore.repository.CartRepository;
 import shop.mybookstore.repository.UserRepository;
 
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -17,8 +17,11 @@ public class UserService {
 
     UserRepository userRepository;
 
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 
-    public Optional<User> getUserById(Long userId){
+    public Optional<User> getUserById(Long userId) {
         return userRepository.findById(userId);
     }
 
@@ -27,16 +30,30 @@ public class UserService {
                            String lastName,
                            String password,
                            String email,
-                           RoleEnum role){
-       User user = new User(username, firstName, lastName, password, email, role);
-       return user;
+                           RoleEnum role) {
+        User user = new User(username, firstName, lastName, password, email, role);
+        return user;
     }
 
-    public void updateUser(Long userId){
-        Optional<User> user = userRepository.findById(userId);
-    }
+    public User updatePassword(Long userId, String currentPassword, String newPassword) {
+        Optional<User> userOptional = userRepository.findById(userId);
 
-    public void deleteUser(Long userId){
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+
+            if (user.getPassword().equals(currentPassword)) {
+                user.setPassword(newPassword);
+                return userRepository.save(user);
+            } else {
+                throw new IllegalArgumentException("Current password is incorrect");
+            }
+        } else {
+                throw new NoSuchElementException("User not found");
+            }
+        }
+
+
+    public void deleteUser(Long userId) {
         userRepository.deleteById(userId);
     }
 
